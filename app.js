c93136dcd60be27862b9baca3180814b27f56add
ERAJ1234 <61812779+ERAJ1234@users.noreply.github.com>
2020-06-21 00:44:44 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,107 @@
+//var firstName=prompt("enter your first name");
+//var lastName=prompt("enter your last name");
+//var fullName = firstName+lastName
+//alert(fullName);
+
+//var fav=prompt("enter your faviroute mobile phone model");
+//var n= fav.length;
+//alert( fav +"the length is"+ n);
+
+
+//var country="pakistan";
+//var ind=country.indexOf("n");
+//alert("the index of letter n is"+" "+ ind);
+
+//var word="hello world";
+//var last=lastCharacterIndex("hellow world");
+//alert(last);
+
+//var country="pakistan"
+//var character=(country.charAt(3))
+//alert("character at index 3 is"+" "+ character);
+
+
+
+//var firstName=prompt("enter your first name");
+//var lastName=prompt("enter your last name");
+//var fullName = firstName.concat(lastName);
+//alert(fullName);
+
+//var word="HYDERABAD";
+//for(var i=0; i<word.length; i++){
+    //if(word.slice(i,4)==="hyder"){
+        //alert(word.slice(0,i)+"islam");
+    //}
+//}
+
+//var messege="ali and sami are bestfriends.They paly cricket and footbal together";
+//var messege1=messege.replace(/and/g,"$");
+//alert(messege1);
+
+//var w="peanuts";
+//var e=w.toUpperCase()
+//alert(e);
+
+//function sentenceCase (str) {
+    //if ((str===null) || (str===''))
+         //return false;
+    //else
+     //str = str.toString();
+  
+   //return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  //}
+  
+  //alert(sentenceCase('javascript'));
+
+
+
+ // userName=propmt("enter a user name");
+ // if(userName==="@"|| userName==="!" || userName==="." ){
+      //alert("you enter a valid code");
+ // }
+  //else{
+     // alert("you entter a in valid code");
+  //}
+
+
+//var item=propmt("enter  a item which you want");
+//var first=item.slice(0,1);
+//var second=item .slice(1);
+//var upper=first.toUpperCase()
+
+
+//var c=first.toUpperCase();
+//var d=second.toLwerCase();
+//var item=first+second
+//alert("item")
+//var fruits =["apple","cake","cookie", "chips","patties"]
+
+//for(var i = 0; i<fruits.length; i++){
+    //if(fruits [i]===item){
+      //  alert("item has found");
+    //}
+  //  else{
+        //alert("item has not found");
+    //}
+//}
+
+
+
+
+//var str = 'university of karachi';
+//var ar = str.split('')
+//for( var i = 0; i< str.length; i++){
+    //alert(ar);
+//}
+
+
+ //var  num=+prompt("enter a number which you want");
+ //var round= Maht.round(num);
+ //var c= Maht.ceil(num);
+ //var f=Maht.floor(num);
+ //alert("number is" + num);
+ //alert("round value is" + round);
+ //alert("ceil value is" + c)
+ //alert("floor value is" + f);
+
+
